17ef43ad85a5359b3530c3e660735bc8ab51d24f
vimwitch <jchancehud@gmail.com>
2024-08-20 17:03:45 -0700

    feat: vector support in r1cs num_ops

--- src/r1cs/mod.rs
+++ src/r1cs/mod.rs
@@ -1,1 +1,2 @@
+mod inv;
 pub mod vm;
--- src/r1cs/vm.rs
+++ src/r1cs/vm.rs
@@ -13,9 +13,23 @@ use std::collections::HashMap;
 // and will be summed
 pub struct R1csConstraint {
     // (coefficient, var_index)
-    pub a: Vec<(u64, u64)>,
-    pub b: Vec<(u64, u64)>,
-    pub c: Vec<(u64, u64)>,
+    pub a: Vec<(u64, usize)>,
+    pub b: Vec<(u64, usize)>,
+    pub c: Vec<(u64, usize)>,
+}
+
+#[derive(Clone, PartialEq, Eq, Hash, Debug)]
+pub enum VarLocation {
+    Static,
+    Constraint,
+}
+
+#[derive(Clone, PartialEq, Eq, Hash, Debug)]
+pub struct Var {
+    index: usize,
+    location: VarLocation,
+    dimensions: Vec<usize>,
+    value: Vec<u64>,
 }
 
 impl ToString for R1csConstraint {
@@ -42,9 +56,9 @@ pub struct VM<'a> {
     pub prime: u64,
     // global counter for distinct variables
     // variable at index 0 is always 1
-    pub var_index: u64,
+    pub var_index: usize,
     // local scope name keyed to global variable index
-    pub vars: HashMap<String, u64>,
+    pub vars: HashMap<String, Var>,
     pub compiler_state: &'a mut CompilerState,
     // a, b, c
     pub constraints: Vec<R1csConstraint>,
@@ -61,7 +75,7 @@ impl<'a> VM<'a> {
         }
     }
 
-    pub fn eval(&mut self, expr: Expr) -> u64 {
+    pub fn eval(&mut self, expr: Expr) -> Var {
         match &expr {
             Expr::VecLit(_v) => {
                 panic!("vector literals must be assigned before operation");
@@ -87,74 +101,184 @@ impl<'a> VM<'a> {
             Expr::NumOp { lhs, op, rhs } => {
                 let lv = self.eval(*lhs.clone());
                 let rv = self.eval(*rhs.clone());
+                if lv.location != VarLocation::Constraint {
+                    log::error!(&format!("lhs is not a constraint variable: {:?}", lhs));
+                }
+                if rv.location != VarLocation::Constraint {
+                    log::error!(&format!("rhs is not a constraint variable: {:?}", rhs));
+                }
+                if rv.dimensions.len() != lv.dimensions.len() {
+                    log::error!(&format!(
+                        "lhs and rhs dimensions are not equal: {:?} {:?}",
+                        lhs, rhs
+                    ));
+                }
+                for x in 0..rv.dimensions.len() {
+                    if rv.dimensions[x] != lv.dimensions[x] {
+                        log::error!(&format!(
+                            "lhs and rhs inner dimensions are not equal: {:?} {:?}",
+                            lhs, rhs
+                        ));
+                    }
+                }
+                // take a lhs and rhs of variable size and apply
+                // an operation to each element
+                let mut operate = |lhs: Var,
+                                   rhs: Var,
+                                   op: Box<
+                    dyn Fn(u64, u64, usize, usize, usize) -> (Vec<R1csConstraint>, u64),
+                >|
+                 -> Var {
+                    let mut new_var = Var {
+                        index: self.var_index,
+                        location: VarLocation::Constraint,
+                        dimensions: lhs.dimensions.clone(),
+                        value: vec![],
+                    };
+                    self.var_index += lhs.value.len();
+                    for x in 0..lhs.value.len() {
+                        // will generate constraints and output value
+                        let (constraints, val) = (*op)(
+                            lhs.value[x],
+                            rhs.value[x],
+                            lhs.index + x,
+                            rhs.index + x,
+                            new_var.index + x,
+                        );
+                        new_var.value.push(val);
+                        for c in constraints {
+                            self.constraints.push(c);
+                        }
+                    }
+                    new_var
+                };
+                // TODO: better field math
                 match op {
                     Op::Add => {
-                        let new_var = self.var_index;
-                        self.var_index += 1;
-                        // (1*lv + 1*rv) * (1*1) - (1*new_var) = 0
-                        // lv + rv - new_var = 0
-                        self.constraints.push(R1csConstraint {
-                            a: vec![(1, lv), (1, rv)],
-                            b: vec![(1, 0)],
-                            c: vec![(1, new_var)],
-                        });
-                        return new_var;
+                        let add = |a: u64,
+                                   b: u64,
+                                   ai: usize,
+                                   bi: usize,
+                                   oi: usize|
+                         -> (Vec<R1csConstraint>, u64) {
+                            let x = (u128::try_from(a).unwrap() + u128::try_from(b).unwrap())
+                                % u128::try_from(self.prime).unwrap();
+                            // (1*lv + 1*rv) * (1*1) - (1*new_var) = 0
+                            // lv + rv - new_var = 0
+                            (
+                                vec![R1csConstraint {
+                                    a: vec![(1, ai), (1, bi)],
+                                    b: vec![(1, 0)],
+                                    c: vec![(1, oi)],
+                                }],
+                                u64::try_from(x).unwrap(),
+                            )
+                        };
+                        operate(lv, rv, Box::new(add))
                     }
                     Op::Mul => {
-                        let new_var = self.var_index;
-                        self.var_index += 1;
-                        // (1*lv) * (1*rv) - (1*new_var) = 0
-                        // lv * rv - new_var = 0
-                        self.constraints.push(R1csConstraint {
-                            a: vec![(1, lv)],
-                            b: vec![(1, rv)],
-                            c: vec![(1, new_var)],
-                        });
-                        return new_var;
+                        let mul = |a: u64,
+                                   b: u64,
+                                   ai: usize,
+                                   bi: usize,
+                                   oi: usize|
+                         -> (Vec<R1csConstraint>, u64) {
+                            let x = (u128::try_from(a).unwrap() * u128::try_from(b).unwrap())
+                                % u128::try_from(self.prime).unwrap();
+                            // (1*lv) * (1*rv) - (1*new_var) = 0
+                            // lv * rv - new_var = 0
+                            (
+                                vec![R1csConstraint {
+                                    a: vec![(1, ai)],
+                                    b: vec![(1, bi)],
+                                    c: vec![(1, oi)],
+                                }],
+                                u64::try_from(x).unwrap(),
+                            )
+                        };
+                        operate(lv, rv, Box::new(mul))
                     }
                     Op::Sub => {
-                        let new_var = self.var_index;
-                        self.var_index += 1;
-                        // (1*lv + -1*rv) * (1*1) - (1*new_var) = 0
-                        // lv + -1*rv - new_var = 0
-                        self.constraints.push(R1csConstraint {
-                            a: vec![(1, lv), (self.prime - 1, rv)],
-                            b: vec![(1, 0)],
-                            c: vec![(1, new_var)],
-                        });
-                        return new_var;
+                        let sub = |a: u64,
+                                   b: u64,
+                                   ai: usize,
+                                   bi: usize,
+                                   oi: usize|
+                         -> (Vec<R1csConstraint>, u64) {
+                            let x = (u128::try_from(self.prime - 1).unwrap()
+                                + u128::try_from(a).unwrap()
+                                - u128::try_from(b).unwrap())
+                                % u128::try_from(self.prime).unwrap();
+                            // (1*lv + -1*rv) * (1*1) - (1*new_var) = 0
+                            // lv + -1*rv - new_var = 0
+                            (
+                                vec![R1csConstraint {
+                                    a: vec![(1, ai), (self.prime - 1, bi)],
+                                    b: vec![(1, 0)],
+                                    c: vec![(1, oi)],
+                                }],
+                                u64::try_from(x).unwrap(),
+                            )
+                        };
+                        operate(lv, rv, Box::new(sub))
                     }
                     Op::Inv => {
                         // (1/rhs) * lhs
-                        //
-                        let rhs_inv = self.var_index;
-                        self.var_index += 1;
-                        // first: constrain rhs_inv
-                        // (1*rhs) * (1*rhs_inv) - (1*1) = 0
-                        // rhs * rhs_inv - 1 = 0
-                        self.constraints.push(R1csConstraint {
-                            a: vec![(1, lv)],
-                            b: vec![(1, rhs_inv)],
-                            c: vec![(1, 0)],
-                        });
-                        let new_var = self.var_index;
-                        self.var_index += 1;
-                        // second: constrain the inv multiplication
-                        // (1*lhs) * (1*rhs_inv) - (1*new_var) = 0
-                        self.constraints.push(R1csConstraint {
-                            a: vec![(1, lv)],
-                            b: vec![(1, rhs_inv)],
-                            c: vec![(1, new_var)],
-                        });
-                        return new_var;
+                        // first invert the rhs and store in a variable
+                        let inv = |_: u64,
+                                   b: u64,
+                                   _: usize,
+                                   bi: usize,
+                                   oi: usize|
+                         -> (Vec<R1csConstraint>, u64) {
+                            let b_inv = crate::r1cs::inv::inv(b, self.prime);
+                            // first: constrain rhs_inv
+                            // (1*rhs) * (1*rhs_inv) - (1*1) = 0
+                            // rhs * rhs_inv - 1 = 0
+                            (
+                                vec![R1csConstraint {
+                                    a: vec![(1, bi)],
+                                    b: vec![(1, oi)],
+                                    c: vec![(1, 0)],
+                                }],
+                                u64::try_from(b_inv).unwrap(),
+                            )
+                        };
+                        let rv_inv = operate(rv.clone(), rv.clone(), Box::new(inv));
+                        // then multiple rv_inv by the lhs
+                        let mul = |a: u64,
+                                   b: u64,
+                                   ai: usize,
+                                   bi: usize,
+                                   oi: usize|
+                         -> (Vec<R1csConstraint>, u64) {
+                            let x = (u128::try_from(a).unwrap() * u128::try_from(b).unwrap())
+                                % u128::try_from(self.prime).unwrap();
+                            // (1*lv) * (1*rv) - (1*new_var) = 0
+                            // lv * rv - new_var = 0
+                            (
+                                vec![R1csConstraint {
+                                    a: vec![(1, ai)],
+                                    b: vec![(1, bi)],
+                                    c: vec![(1, oi)],
+                                }],
+                                u64::try_from(x).unwrap(),
+                            )
+                        };
+                        operate(lv, rv_inv, Box::new(mul))
                     }
                 }
             }
             Expr::Lit(val) => {
-                let new_var = self.var_index;
+                let new_var = Var {
+                    index: self.var_index,
+                    location: VarLocation::Constraint,
+                    dimensions: vec![],
+                    value: vec![*val],
+                };
                 self.var_index += 1;
                 self.constraints.push(R1csConstraint {
-                    a: vec![(1, new_var)],
+                    a: vec![(1, new_var.index)],
                     b: vec![(1, 0)],
                     c: vec![(val.clone(), 0)],
                 });
