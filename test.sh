88d4ef9d2d380a302c8de2f26926c351b73feefd
vimwitch <jchancehud@gmail.com>
2024-08-23 19:07:24 -0700

    chore: disable r1cs test in tasm

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -e
 
 for entry in test-vectors/*
 do
-  if echo $entry | grep "_test.ash"
+  if echo $entry | grep "_test.ash" | grep -v "r1cs"
   then
 
     cargo run --release -- -t tasm $(basename $entry | sed "s/.ash//") -i ./stdlib -i ./test-vectors -v -p 1 -s 1
