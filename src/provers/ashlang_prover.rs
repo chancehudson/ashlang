b8f8e145a73ccda77b232ac231b0ce62d66aa82c
vimwitch <jchancehud@gmail.com>
2024-09-05 21:45:43 -0700

    chore: AshlangProver comments

--- src/provers/ashlang_prover.rs
+++ src/provers/ashlang_prover.rs
@@ -4,6 +4,10 @@ use crate::cli::Config;
 
 // TODO: make the resulting proof serializable here
 // e.g. `AshlangProver<T: Debug>`
+
+/// A trait representing an abstract prover implementation.
+/// The generic argument indicates the type of the proof that
+/// the prover builds.
 pub trait AshlangProver<T> {
     fn prove(config: &Config) -> Result<T>;
     fn verify(proof: T) -> Result<bool>;
